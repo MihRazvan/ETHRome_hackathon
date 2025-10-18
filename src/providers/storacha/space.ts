fd68a7ea8875cefc28e4b5de1c9461456b4a1294
RAZVAN MIHAILESCU <RAZVAN.MIHAILESCU1996@GMAIL.COM>
2025-10-18 12:43:00 +0200

    Add Storacha proof, receipt handling, and shared space management, including error handling and delegation parsing.

---
+++ src/providers/storacha/space.ts
@@ -0,0 +1,46 @@
+import type { Capabilities, Fact } from '@ucanto/client'
+import { DID } from '@ucanto/core/schema'
+
+type Delegation = { facts: Fact[]; capabilities: Capabilities }
+
+type SharedSpaceModel = {
+  id: `did:key:${string}`
+  delegation: Delegation
+  meta: { name?: string }
+}
+
+export class SharedSpace {
+  model: SharedSpaceModel
+  constructor(model: SharedSpaceModel) {
+    this.model = model
+  }
+
+  get meta() {
+    return this.model.meta
+  }
+
+  did() {
+    return this.model.id
+  }
+}
+
+export const fromDelegation = ({
+  facts,
+  capabilities,
+}: Delegation): SharedSpace => {
+  const result = DID.match({ method: 'key' }).read(capabilities[0].with)
+  if (result.error) {
+    throw new Error(
+      `Invalid delegation, expected capabilities[0].with to be DID, ${result.error}`,
+      { cause: result.error },
+    )
+  }
+
+  const meta: { name?: string } = facts[0]?.space ?? {}
+
+  return new SharedSpace({
+    id: result.ok,
+    delegation: { facts, capabilities },
+    meta,
+  })
+}
